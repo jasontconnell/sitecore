eedf845a0c94a4796a9954608ca2f15759f5327c
Jason Connell <jconnell@delphicdigital.com>
2018-05-11 22:53:22 -0400

    Add filter item map

--- api/tree.go
+++ api/tree.go
@@ -3,6 +3,8 @@ package api
 import (
 	"github.com/google/uuid"
 	"github.com/jasontconnell/sitecore/data"
+	"path"
+	"strings"
 )
 
 var rootID uuid.UUID = MustParseUUID("00000000-0000-0000-0000-000000000000")
@@ -26,6 +28,34 @@ func LoadFieldMap(list []*data.FieldValue) data.FieldValueMap {
 		m[fv.ItemID] = l
 	}
 	return m
+}
+
+func FilterItemMap(m data.ItemMap, paths []string) data.ItemMap {
+	filteredMap := make(data.ItemMap)
+	for _, item := range m {
+		include := false
+		for _, b := range paths {
+			negate := b[0] == '-'
+			b := strings.TrimPrefix(b, "-")
+
+			if !include && strings.HasPrefix(item.GetPath(), b) {
+				include = !negate
+				break
+			} else {
+				parent := path.Dir(b)
+				for parent != "/" && parent != "" && !include {
+					include = item.GetPath() == parent
+					parent = path.Dir(b)
+				}
+			}
+		}
+
+		if include {
+			filteredMap[item.GetId()] = item
+		}
+	}
+
+	return filteredMap
 }
 
 func getMap(list []data.ItemNode) (root data.ItemNode, m data.ItemMap) {
